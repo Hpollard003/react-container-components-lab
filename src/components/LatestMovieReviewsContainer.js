c0c04577b7dd5b9c6c23b3da318ed85f79004ae6
Hpollard003 <hpollard003@gmail.com>
2021-06-21 14:14:22 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,23 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+        reviews: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch(URL)
+        .then(res => res.json())
+        .then(response => this.setState({ reviews: response.results }));
+    }
+  
+}
+  
+  export default LatestMovieReviewsContainer;
